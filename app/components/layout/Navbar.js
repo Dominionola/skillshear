4f5fa799f637aae68707e190226379df3c282143
unknown <theoladejitoluwa@gmail.com>
2025-09-28 20:10:26 +0100

    auth page arangment

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className=" fixed w-full shadow-md bg-white">
+    <nav className=" fixed w-full shadow-md bg-white flex items-center justify-between h-16 font-sans">
       <div className=" flex px-5">
 
         <Link className="flex items-center " href="/">
@@ -12,8 +12,15 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div>
-        
+      <div className="hidden md:flex gap-4 px-5">
+        <Link href="/" className="text-gray-600">Home</Link>
+        <Link href="/about" className="text-gray-600">About</Link>
+        <Link href="/post" className="text-gray-600">Posts</Link>
+      </div>
+
+      <div className="flex">
+<Link>Login</Link>
+<Link className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-md" href="/signup">Sign Up</Link>
       </div>
     </nav>
   );
