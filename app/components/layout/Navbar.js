d0dd6f578915359ed4078b032d40a2f687c897d9
unknown <theoladejitoluwa@gmail.com>
2025-09-29 06:16:37 +0100

    birthday

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 import React, { useState, useRef, useEffect } from "react";
 
 export default function Navbar() {
-
   const [menuOpen, setMenuOpen] = useState(false);
   const menuRef = useRef(null);
   const buttonRef = useRef(null);
@@ -35,40 +34,60 @@ export default function Navbar() {
 
   return (
     <nav className="font-sans fixed w-full shadow-md bg-white flex items-center justify-between h-16 ">
-     <div className="w-full flex justify-between items-center max-w-7xl mx-auto"> 
-      <div className=" flex px-5">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <div className=" flex px-5">
+          <Link className="flex items-center " href="/">
+            <Image src="/logo.png" alt="Logo" width={50} height={100} />
+            <h2 className="text-2xl font-bold text-black">SkillShare</h2>
+          </Link>
+        </div>
 
-        <Link className="flex items-center " href="/">
-          <Image src="/logo.png" alt="Logo" width={50} height={100} />
-          <h2 className="text-2xl font-bold text-black">SkillShare</h2>
-        </Link>
-      </div>
+        <div className="hidden md:flex gap-4 lg:gap-5 px-5">
+          <Link href="/" className="text-gray-600">
+            Home
+          </Link>
+          <Link href="/about" className="text-gray-600">
+            About
+          </Link>
+          <Link href="/post" className="text-gray-600">
+            Posts
+          </Link>
+        </div>
 
-      <div className="hidden md:flex gap-4 lg:gap-5 px-5">
-        <Link href="/" className="text-gray-600">Home</Link>
-        <Link href="/about" className="text-gray-600">About</Link>
-        <Link href="/post" className="text-gray-600">Posts</Link>
-      </div>
+        <div className="hidden md:flex items-center px-6 lg:gap-2">
+          <Link className="text-gray-600" href="/auth/login">
+            Login
+          </Link>
+          <Link
+            className="ml-4 bg-blue-600 text-white px-4 py-2 rounded-3xl"
+            href="/auth/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
 
-      <div className="hidden md:flex items-center px-6 lg:gap-2">
-<Link className="text-gray-600" href="/auth/login">Login</Link>
-<Link className="ml-4 bg-blue-600 text-white px-4 py-2 rounded-3xl" href="/auth/signup">Sign Up</Link>
-      </div>
-
-      <button 
-      ref={buttonRef}
+        <button
+          ref={buttonRef}
           className="lg:hidden cursor-pointer"
-          onClick={() => setMenuOpen(!menuOpen)}>
-<Image className="md:hidden mr-4" src="/icons/list.svg" alt="Menu" width={24} height={24} />
-      </button>
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <Image
+            className="md:hidden mr-4"
+            src="/icons/list.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+          />
+        </button>
       </div>
 
-       <ul
-        ref={menuRef}
-        className={`fixed flex flex-col top-0 bottom-0 right-0 w-64 bg-white transition-transform duration-500 px-10
+<div className={`fixed flex flex-col justify-between top-0 bottom-0 right-0 w-100 bg-white text-black transition-transform duration-500 px-10
           gap-6 py-16 h-screen z-50 ${
             menuOpen ? "translate-x-0" : "translate-x-full"
-          }`}
+          }`}>
+      <ul
+        ref={menuRef}
+        className="flex flex-col gap-4"
       >
         <div
           className="absolute top-6 right-10"
@@ -87,7 +106,7 @@ export default function Navbar() {
           <Link
             href="/"
             onClick={() => setMenuOpen(false)}
-           className=" font-medium"
+            className=" font-medium"
           >
             Home
           </Link>
@@ -111,9 +130,30 @@ export default function Navbar() {
             Post
           </Link>
         </li>
-       
+        </ul>
         
-      </ul>
+
+
+        <div className="flex flex-col justify-center gap-4 ">
+          <Link
+            href="/auth/signup"
+            onClick={() => setMenuOpen(false)}
+            className="ml-4 bg-blue-600 text-white text-center px-4 py-2 rounded-3xl"
+          >
+            Sign Up
+          </Link>
+         
+          <Link
+            href="/auth/login"
+            onClick={() => setMenuOpen(false)}
+            className=" ml-4 border-1 border-gray-300 text-blue-600 text-center px-4 py-2 rounded-3xl"
+          >
+            Login
+          </Link>
+         
+        </div>
+        </div>
+      
     </nav>
   );
 }
