ce713c4aadf2b11725ed58af5cc67ea33405e004
unknown <theoladejitoluwa@gmail.com>
2025-10-01 02:01:15 +0100

    time to sleep

--- app/components/layout/HeroSection.js
+++ app/components/layout/HeroSection.js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <section className="hero-section max-w-7xl mx-auto text-center">
@@ -7,13 +10,39 @@ export default function HeroSection() {
           <span className="text-blue-500 font-serif italic font-medium font-swash text-5xl tracking-tighter">
             Growth
           </span>{" "}
-          with Shared Expertise
+          with Shared{" "}
+          <span className="text-blue-500 font-serif italic font-medium font-swash text-5xl tracking-tighter">
+            Expertiese
+          </span>
         </h1>
-        <p className="mt-4 text-lg">
+        <p className="mt-4 text-lg text-gray-600e">
           Connect with learners and experts worldwide. Share your knowledge,
           discover new skills, and grow together in our vibrant learning
           community.
         </p>
+        <div className="flex justify-center space-x-4">
+          <Link
+            href="/signup"
+            className="mt-6 inline-flex items-center bg-blue-600 text-white px-6 py-3 rounded-3xl text-lg font-medium hover:bg-blue-700 transition-colors"
+          >
+            <span>Explore Now</span>
+            <Image
+              src="/icons/arrow-right.svg"
+              alt="Arrow icon"
+              width={16}
+              height={16}
+              className="ml-2"
+            />
+          </Link>
+
+           <Link
+            href="/signup"
+            className="mt-6 inline-flex items-center bg-white border-1 border-gray-400 text-black px-6 py-3 rounded-3xl text-lg font-medium hover:bg-blue-700 transition-colors"
+          >
+            <span>Explore Now</span>
+           
+          </Link>
+        </div>
       </div>
     </section>
   );
