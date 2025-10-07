d246b64fe1c1c48753be8b696b0f4f3cc6aec9e8
unknown <theoladejitoluwa@gmail.com>
2025-10-07 16:10:38 +0100

    lal

--- app/components/layout/HeroSection.js
+++ app/components/layout/HeroSection.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="hero-section bg-gray-200">
-      <div className="flex flex-col md:flex-row mx-auto text-center max-w-7xl p-8 md:p-16 gap-8 md:gap-16 items-center justify-center">
+    <section className="hero-section  bg-no-repeat bg-center bg-white-100 h-80vh">
+      <div className="flex flex-col md:flex-row  mx-auto text-center max-w-7xl p-8 md:px-16 gap-8 md:gap-16 items-center justify-center">
         <div className=" justify-center items-center flex flex-col md:items-start md:text-left max-w-2xl">
-          <h1 className="text-4xl font-bold font-sans">
+          < h1 className="text-4xl font-bold font-sans text-center">
             Empowering Your{" "}
             <span className="text-blue-500 font-serif italic font-medium font-swash text-5xl tracking-tighter">
               Growth
@@ -16,12 +16,12 @@ export default function HeroSection() {
               Expertiese
             </span>
           </h1>
-          <p className="mt-4 text-lg text-gray-600">
+          <p className="mt-4 text-lg text-gray-600 text-center">
             Connect with learners and experts worldwide. Share your knowledge,
             discover new skills, and grow together in our vibrant learning
             community.
           </p>
-          <div className="flex flex-col md:flex-row md:gap-4 items-center justify-center ">
+          <div className="flex flex-col w-full md:flex-row md:gap-4 md:text-center items-center justify-center ">
             <Link
               href="/auth/signup"
               className="mt-6 inline-flex items-center justify-center md:max-w-90 bg-blue-600 text-white px-6 py-3 rounded-3xl text-lg font-medium hover:bg-blue-700 transition-colors whitespace-nowrap"
@@ -32,7 +32,7 @@ export default function HeroSection() {
                 alt="Arrow icon"
                 width={16}
                 height={16}
-                className="ml-2"
+                className="ml-2 rounded-xl"
               />
             </Link>
 
@@ -44,17 +44,16 @@ export default function HeroSection() {
             </Link>
           </div>
         </div>
-        <div>
-          <Image
-            src="/assets/hero-img.png"
-            alt="Hero Image"
-            width={800}
-            height={400}
-            className="mt-8 mx-auto"
-          />
-        </div>
       </div>
-      <div></div>
+      <div className="mt-8 mx-auto rounded-3xl overflow-hidden w-[900px] max-w-full">
+        <Image
+          src="/assets/hero.png"
+          alt="Hero Image"
+          width={900}
+          height={400}
+          className="w-full h-auto"
+        />
+      </div>
     </section>
   );
 }
