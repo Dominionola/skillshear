9d99c897511d0ee076c3be3a76cd2eca7786c8f6
unknown <theoladejitoluwa@gmail.com>
2025-10-10 17:09:36 +0100

    @

--- app/components/layout/HeroSection.js
+++ app/components/layout/HeroSection.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+// import React, {useState} from "react";
 
 export default function HeroSection() {
+  // const [heartOpen, setHeartOpen] = useState(false);
+
   return (
     <section className="hero-section  bg-no-repeat bg-center bg-white-100 h-80vh">
       <div className="flex flex-col md:flex-row  mx-auto text-center max-w-7xl p-8 md:px-16 gap-8 md:gap-16 items-center justify-center">
@@ -44,46 +47,68 @@ export default function HeroSection() {
             </Link>
           </div>
 
-          <ul className="flex flex-wrap">
-           
+          <div className="text-center w-full">
+            <ul className="flex -space-x-3 mt-8 text-center items-center justify-center group">
+              <li className="inline-block rounded-full overflow-hidden w-7 h-7 align-middle border-2 border-white">
+                <Image
+                  src="/assets/4thWO.jpg"
+                  alt="users"
+                  width={28}
+                  height={28}
+                  className="w-full h-full object-cover"
+                />
+              </li>
+              <li className="inline-block rounded-full overflow-hidden w-7 h-7 align-middle border-2 border-white">
+                <Image
+                  src="/assets/3rdWO.jpg"
+                  alt="users"
+                  width={28}
+                  height={28}
+                  className="w-full h-full object-cover"
+                />
+              </li>
+              <li className="inline-block rounded-full overflow-hidden w-7 h-7 align-middle border-2 border-white">
+                <Image
+                  src="/assets/2ndWO.jpg"
+                  alt="users"
+                  width={28}
+                  height={28}
+                  className="w-full h-full object-cover"
+                />
+              </li>
+              <li className="inline-block rounded-full overflow-hidden w-7 h-7 align-middle border-2 border-white">
+                <Image
+                  src="/assets/1stWO.jpg"
+                  alt="users"
+                  width={28}
+                  height={28}
+                  className="w-full h-full object-cover"
+                />
+              </li>
 
-            <li className="mt-8 inline-block mr-2 rounded-full overflow-hidden w-7 h-7 align-middle">
-              <Image
-                src="/assets/4thWO.jpg"
-                alt="users"
-                width={28}
-                height={28}
-                className="w-full h-full object-cover"
-              />
-            </li>
-            <li className="mt-8 inline-block mr-2 rounded-full overflow-hidden w-7 h-7 align-middle">
-              <Image
-                src="/assets/3rdWO.jpg"
-                alt="users"
-                width={28}
-                height={28}
-                className="w-full h-full object-cover"
-              />
-            </li>
-            <li className="mt-8 inline-block mr-2 rounded-full overflow-hidden w-7 h-7 align-middle">
-              <Image
-                src="/assets/2ndWO.jpg"
-                alt="users"
-                width={28}
-                height={28}
-                className="w-full h-full object-cover"
-              />
-            </li>
-            <li className="mt-8 inline-block mr-2 rounded-full overflow-hidden w-7 h-7 align-middle">
-              <Image
-                src="/assets/1stWO.jpg"
-                alt="users"
-                width={28}
-                height={28}
-                className="w-full h-full object-cover"
-              />
-            </li>
-          </ul>
+              <div className="relative flex flex-col items-center pl-5">
+                <span className="absolute -top-4 left-1/2 -translate-x-1/2 flex gap-1 opacity-0 group-hover:opacity-100 group-hover:animate-heart-rise">
+                  <Image
+                    src="/icons/heart.svg"
+                    alt="Heart icon"
+                    width={16}
+                    height={16}
+                    className="inline-block rotate-[-12deg] "
+                  />
+                  <Image
+                    src="/icons/heart.svg"
+                    alt="Heart icon"
+                    width={16}
+                    height={16}
+                    className="inline-block mb-1 -mt-2 rotate-12"
+                  />
+                </span>
+                <span className="text-sm text-gray-700">
+                  Loved by over 1,000 learners worldwide
+                </span>
+              </div>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="mt-8 mx-auto rounded-3xl overflow-hidden w-[900px] max-w-full">
@@ -92,7 +117,7 @@ export default function HeroSection() {
           alt="Hero Image"
           width={900}
           height={400}
-          className="w-full h-auto"
+          className="w-full h-auto "
         />
       </div>
     </section>
