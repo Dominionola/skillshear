fa3e217c963f6fd17c13f4426c1ea422ff799812
unknown <theoladejitoluwa@gmail.com>
2025-10-11 06:05:48 +0100

    lala

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import HeroSection from "./components/layout/HeroSection";
 import Footer from "./components/layout/Footer";
+import FeatureSection from "./components/layout/FeatureSection";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <div className="w-screen relative left-1/2 right-1/2 -mx-[50vw]">
           <HeroSection />
         </div>
-        <HeroSection />
+        <FeatureSection />
       </main>
       <Footer />
     </div>
