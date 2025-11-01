1fb6681994a3a61716c2e1302aae711aa3ea303f
unknown <theoladejitoluwa@gmail.com>
2025-11-01 07:31:09 +0100

    haapy new month

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -1,10 +1,14 @@
+
+
 export default function SignUpPage() {
   return (
-    <div className="pt-16 flex justify-center items-center min-h-screen">
-      <div>
-        <h1 className="text-2xl ">Sign Up to Join a Community you Love</h1>
-        <div>
-            
+    <div className="pt-16 flex justify-center items-center min-h-screen ">
+      <div className="w-full px-4">
+        <div className="max-w-3xl mx-auto">
+          <h1 className="text-2xl md:text-3xl font-semibold mb-6">
+            Sign Up to Join a Community you Love
+          </h1>
+          
         </div>
       </div>
     </div>
